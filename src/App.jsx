c65922ccb5160a404fbd5422bd0585c8383524b7
Hammed <hammedusman812@gmail.com>
2024-06-28 23:47:04 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import ForeCast from './ForeCast/ForeCast'
+const App = () => {
+  return (
+<>
+   <ForeCast/>
+    </>
+  )
+}
+
+export default App
